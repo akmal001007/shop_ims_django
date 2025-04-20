3f3c0b756dab52108c6d4488cdfa539dd2c3f09d
Akmal Khan <akmalnawabi007@gmail.com>
2025-04-20 16:46:41 +0430

    new changes

--- shopims/urls.py
+++ shopims/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', lambda request: redirect('admin/')),
     path('admin/', admin.site.urls),
     path('', include('inventory.urls')),  # ✅ This includes all your app routes
 ]
