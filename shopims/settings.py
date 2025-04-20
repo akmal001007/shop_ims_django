ecb92385696d5fcc783676f94b4cb7d9c99292f6
Akmal Khan <akmalnawabi007@gmail.com>
2025-04-20 16:37:33 +0430

    for deploy

--- shopims/settings.py
+++ shopims/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,8 +26,9 @@ SECRET_KEY = 'django-insecure-==a4s54y(qrd#k#f!xv$@=*ra&o#f1!39_^9-#)&ql7&neiu(p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Application definition
 
